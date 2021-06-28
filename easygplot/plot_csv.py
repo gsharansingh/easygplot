01805d1cde0995e12968c672b9db2273c3b221ba
Gursharan Singh <ggurs096@uottawa.ca>
2021-06-28 00:41:38 -0400

    adding initial build of hist, box in the plot_csv

--- easygplot/plot_csv.py
+++ easygplot/plot_csv.py
@@ -8,8 +8,20 @@ def pie_plot(data, labels):
     plt.show()
 
 def bar_plot(data, labels):
-    plt.bar(data,height = max(data), label = labels)
-    plt.show()   
+    plt.bar(labels, height = data, tick_label = labels)
+    plt.xticks(rotation=90)
+    plt.show()
+
+def box_plot(data, labels = None):
+    plt.boxplot(data, labels = labels)
+    plt.xticks(rotation=90)
+    plt.show()
+
+def hist_plot(data, bins = None, labels = None):
+    plt.hist(data, bins = 10, label = labels)
+    plt.subplots_adjust(right=0.75)
+    plt.legend(labels, loc='upper right', bbox_to_anchor=(0.4, 0, 1, 1))
+    plt.show()
 
 def plot_csv(filename, x_column = None, y_column = 1, title = None, legend=False, linestyle = 'solid', other_graphs = False):
     try:
@@ -44,12 +56,29 @@ def plot_csv(filename, x_column = None, y_column = 1, title = None, legend=False
         plt.ylabel("Price")
     if legend:
         plt.legend(fields[1:], loc='upper right', bbox_to_anchor=(0.4, 0, 1, 1))
+    plt.xticks(rows[:, 0], rotation=90)
     plt.show()
 
-    if 'pie' in other_graphs:
+    if 'all' in other_graphs:
+        hist_plot(rows[:,1:], labels = fields[1:])
+        box_plot(rows[:,1:], labels = fields[1:])
         pie_plot(np.sum(rows[:, 1:], axis = 1), labels = rows[:, 0])
         pie_plot(np.sum(rows[:,1:], axis = 0), labels = fields[1:])
-
-    if 'bar' in other_graphs:
         bar_plot(np.sum(rows[:, 1:], axis = 1), labels = rows[:, 0])
         bar_plot(np.sum(rows[:,1:], axis = 0), labels = fields[1:])
+
+    else:
+
+        if 'hist' in other_graphs:
+            hist_plot(rows[:,1:], labels = fields[1:])
+
+        if 'box' in other_graphs:
+            box_plot(rows[:,1:], labels = fields[1:])
+
+        if 'pie' in other_graphs:
+            pie_plot(np.sum(rows[:, 1:], axis = 1), labels = rows[:, 0])
+            pie_plot(np.sum(rows[:,1:], axis = 0), labels = fields[1:])
+
+        if 'bar' in other_graphs:
+            bar_plot(np.sum(rows[:, 1:], axis = 1), labels = rows[:, 0])
+            bar_plot(np.sum(rows[:,1:], axis = 0), labels = fields[1:])
