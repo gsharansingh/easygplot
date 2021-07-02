8c57aee4d5928edda2398a63d9994298e1fcd50a
Gursharan Singh <ggurs096@uottawa.ca>
2021-07-01 21:49:06 -0400

    created a plot_line module to achieve multiple object support

--- easygplot/plot_csv.py
+++ easygplot/plot_csv.py
@@ -6,33 +6,6 @@ from sklearn.impute import SimpleImputer
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-class line(object):
-    def __init__ (self, figsize = (8, 5), linestyle = 'solid', legend = False):
-        self.figsize = figsize
-        self.linestyle = linestyle
-        self.legend = legend
-
-    def __call__ (self, sample):
-        data, labels = sample
-        plt.figure(figsize=self.figsize)
-        plt.plot(labels[0], data, linestyle = self.linestyle)
-        plt.subplots_adjust(left=None, bottom=None, right=0.75, top=None, wspace=None, hspace=None)
-        # if title:
-        #     plt.title(title)
-        # if x_label:
-        #     plt.xlabel(x_label)
-        # if y_label:
-        #     plt.ylabel(y_label)
-        if self.legend:
-            if type(labels[1]) == str:
-                plt.legend([labels[1]], loc='upper right', bbox_to_anchor=(0.4, 0, 1, 1))
-            else:
-                plt.legend(labels[1], loc='upper right', bbox_to_anchor=(0.4, 0, 1, 1))
-        plt.xticks(labels[0], rotation=90)
-        plt.show()
-
-    
-
 def line_plot(data, labels, x_label = None, y_label = None, title = None, legend=False, linestyle = 'solid'):
     plt.plot(labels[0], data, linestyle = linestyle)
     plt.subplots_adjust(left=None, bottom=None, right=0.75, top=None, wspace=None, hspace=None)
--- test.py
+++ test.py
@@ -12,17 +12,17 @@ print(data.head())
 import easygplot
 easygplot.plot(data.iloc[:, 0], data.iloc[:, 1:], title = "Gas Prices", legend = list(data.columns[1:]))
 
-from easygplot import plot_csv, read
+from easygplot import plot_csv, read, plot_line
 mydata = read.CSV("gasprices.csv")
 # print(mydata[:])
 # print(mydata)
 # print(len(mydata))
 
-line_plot = plot_csv.line(legend=True)
-# line_plot(mydata[:])
+line_plot = plot_line.plot_line(legend=True)
+line_plot(mydata[:])
 # l1, l2 = mydata[3]
 # print(l2[1])
-plot_csv.line(legend=True)(mydata[1])
+plot_line.plot_line(legend=True)(mydata[1])
 
 # print([data for data in mydata])
 
